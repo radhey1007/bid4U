40d899f079abb25f222dbe346bc32069817a64ff
bitbucket-villagerboy <ankitshukla1992@outlook.com>
2019-10-16 02:22:19 +0530

    post job

--- src/app/admin/quiz/quiz.component.ts
+++ src/app/admin/quiz/quiz.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-quiz',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quiz.component.css']
 })
 export class QuizComponent implements OnInit {
-
+  quiz = {
+    answerE: '',
+    answerD: '',
+    answerC: '',
+    answerB: '',
+    answerA: '',
+    question: '',
+    series: '',
+    subject: ''
+  }
+  @ViewChild('myForm', { static: false }) mytemplateForm: NgForm;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  postQuiz = () => {
+    this.mytemplateForm.reset();
   }
 
 }
